9303370a526a47ce511a07fad89d5653c53177d5
Peter Polyakov <polyakovpetr@mail.ru>
2021-04-02 14:21:04 +0300

    Update config.py

--- webapp/config.py
+++ webapp/config.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 CKEDITOR_FILE_UPLOADER = 'upload'
-UPLOADED_PATH = os.getenv('UPLOADED_PATH')
+UPLOADED_PATH = os.path.join(basedir, 'uploads')
